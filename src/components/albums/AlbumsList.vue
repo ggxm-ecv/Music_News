f5387977c86fbccfe267f14c5a8fc90da632d6fa
Gautier <hello.gautiermaire@gmail.com>
2021-05-02 23:20:18 +0200

    tabs on home page

---
+++ src/components/albums/AlbumsList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h2> Les dernièrs albums </h2>
+    <id-album
+      v-for="album in albums" 
+      :key="album.id" 
+      :album="album"
+    ></id-album>
+  </div>
+</template>
+
+<script>
+import IdAlbum from "@/components/albums/IdAlbum"
+import axios from 'axios'
+
+export default {
+  name: 'AlbumsList',
+  data () {
+    return {
+      albums: [],
+    }
+  },
+  components: {
+    'id-album': IdAlbum
+  },
+  methods: {
+    async fetchData () {
+      const token = localStorage.getItem('vuejs_token')
+
+      const res = await axios.get('http://localhost:3000/albums', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      this.albums = res.data
+    }
+  },
+  mounted () {
+    this.fetchData()
+  }
+}
+</script>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -4,19 +4,29 @@
 
     <h1> Accueil </h1>
 
-    <nav>
+    <nav class="">
       <ul class="home__nav">
-        <li>Actualités</li>
-        <li>Albums</li>
+        <li class="home__nav-item active"
+            cat-name="news"
+            @click="switchCat"
+        >
+          Actualités
+        </li>
+        <li class="home__nav-item"
+            cat-name="albums"
+            @click="switchCat"
+        >
+          Albums
+        </li>
       </ul>
     </nav>
     
-    <div class="news">
+    <div class="home__cat news active">
       <NewsList />
     </div>
 
-    <div class="albums">
-
+    <div class="home__cat albums">
+      <AlbumsList />
     </div>
     
     <Footer />
@@ -27,12 +37,70 @@
 import Header from '@/components/static/Header'
 import Footer from '@/components/static/Footer'
 import NewsList from '@/components/news/NewsList'
+import AlbumsList from '@/components/albums/AlbumsList'
 
 export default {
   components: {
     Header,
     Footer,
-    NewsList
+    NewsList,
+    AlbumsList
+  },
+  methods: {
+    switchCat: function (e) {
+      const navItemSelected = e.target
+      const catName = navItemSelected.getAttribute('cat-name')
+
+      document.getElementsByClassName('home__nav-item').forEach(element => {
+        element.classList.remove('active')
+      })
+
+      e.target.classList.add('active')
+
+      document.getElementsByClassName('home__cat').forEach(element => {
+        element.classList.remove('active')
+        if (element.classList.contains(`${catName}`)) {
+          element.classList.add('active')
+        }
+      })
+    }
   }
 }
 </script>
+
+<style>
+
+  .home__nav {
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+    width: 200px;
+    margin: 0 auto;
+    padding: 0;
+  }
+
+  .home__nav-item {
+    cursor: pointer;
+    padding: 8px 16px;
+    border: 2px solid #222;
+    border-radius: 4px;
+    background-color: #fff;
+    color: #222;
+    transition: all 150ms ease-out;
+  }
+
+  .home__nav-item.active,
+  .home__nav-item:hover {
+    background-color: #222;
+    color: #fff;
+  }
+
+  .home__cat {
+    display: none;
+  }
+
+  .home__cat.active {
+    display: block;
+  }
+
+</style>
