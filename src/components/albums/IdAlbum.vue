9b1fcbcaf31abf18fd596ab3f2ae27917a42ecce
Vincent <31652038+Vincent-Manach@users.noreply.github.com>
2021-05-17 20:28:01 +0100

    Add artists in albums and artists in concerts / start pagination

--- src/components/albums/IdAlbum.vue
+++ src/components/albums/IdAlbum.vue
@@ -3,11 +3,12 @@
     <p> {{ album.name }} </p>
     <p> {{ album.released }} </p>
     <p> Nombre de musique : {{ album.tracks }} </p>
-    <p> Id artiste/groupe : {{ album.artistId }} </p>
+    <p> Artiste : {{ artists[this.album.artistId-1].name }} </p>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   props: {
@@ -16,6 +17,26 @@ export default {
       required: true,
       default: () => []
     },
+  },
+  data () {
+    return {
+      artists: [],
+    }
+  },
+  methods: {
+    async fetchData () {
+      const token = localStorage.getItem('vuejs_token')
+
+      const res = await axios.get('http://localhost:3000/artists', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      this.artists = res.data
+    }
+  },
+  mounted () {
+    this.fetchData()
   }
 }
 </script>
--- src/components/concerts/IdConcert.vue
+++ src/components/concerts/IdConcert.vue
@@ -2,18 +2,40 @@
   <div class="id-card">
     <p> Nom : {{ concert.name }} </p>
     <p> Date : {{concert.date}} </p>
-    <p> Artiste id : {{concert.artistId}} </p>
+    <p> Artiste : {{ artists[this.concert.artistId-1].name }} </p>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   props: {
     concert: {
       type: Object,
       required: true,
       default: () => []
-    },
+    }
+  },
+  data () {
+    return {
+      artists: [],
+    }
+  },
+  methods: {
+    async fetchData () {
+      const token = localStorage.getItem('vuejs_token')
+
+      const res = await axios.get('http://localhost:3000/artists', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      this.artists = res.data
+    }
+  },
+  mounted () {
+    this.fetchData()
   }
 }
 </script>
